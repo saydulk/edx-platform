8f948f2939e6510238a9e5b6949ef4961c14bd91
Ned Batchelder <ned@edx.org>
2015-06-26 07:04:11 -0400

    WIP: testing is_edx_domain

--- openedx/core/djangoapps/theming/test_util.py
+++ openedx/core/djangoapps/theming/test_util.py
@@ -4,6 +4,9 @@ Test helpers for Comprehensive Theming.
 
 from functools import wraps
 
+from mock import patch
+
+from django.conf import settings
 from django.test.utils import override_settings
 
 import edxmako
@@ -33,3 +36,43 @@ def with_comp_theme(theme_dir):
                     return func(*args, **kwargs)
         return _decorated
     return _decorator
+
+
+def with_is_edx_domain(is_edx_domain):
+    """
+    A decorator to run a test as if IS_EDX_DOMAIN is true or false.
+
+    We are transitioning away from IS_EDX_DOMAIN and are moving toward an edX
+    theme. This decorator changes both settings to let tests stay isolated
+    from the details.
+
+    Arguments:
+        is_edx_domain (bool): are we an edX domain or not?
+
+    """
+
+    def _decorator(func):                       # pylint: disable=missing-docstring
+        if is_edx_domain:
+            func = with_comp_theme(settings.REPO_ROOT / "themes" / "edx.org")(func)
+        func = patch.dict('django.conf.settings.FEATURES', {"IS_EDX_DOMAIN": is_edx_domain})(func)
+        return func
+    return _decorator
+
+
+
+def dump_theming_info():
+    import os, os.path
+    for namespace, lookup in edxmako.LOOKUP.items():
+        print "--- %s: %s" % (namespace, lookup.template_args['module_directory'])
+        for directory in lookup.directories:
+            print "  %s" % (directory,)
+
+    print "="*80
+    for dir, dirs, files in os.walk(settings.MAKO_MODULE_DIR):
+        print "%s ----------------" % (dir,)
+        for file in sorted(files):
+            if file.endswith(".pyc"):
+                continue
+            with open(os.path.join(dir, file)) as f:
+                bytes = len(f.read())
+            print "    %s: %d" % (file, bytes)
